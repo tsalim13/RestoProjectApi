8da49cfa338400029ce482d77f119ca69850d7df
tsalim13 <gotlm13@gmail.com>
2021-12-05 00:22:59 +0100

    setting migration

---
+++ database/migrations/2021_12_04_225758_create_setting_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('setting', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('key', 60);
+            $table->string('value', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('setting');
+    }
+}
